5ad7b89ae369de25d201147e49654086f51bcb73
Akshat171 <81281246+Akshat171@users.noreply.github.com>
2022-10-21 09:12:48 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ import Main from "../components/Main";
 import Navbar from "../components/Navbar";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
-// import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
