fa07178c575a517054378bebfd54152dfc61e61c
Akshat171 <81281246+Akshat171@users.noreply.github.com>
2022-10-21 09:15:31 +0530

    Update Projects.js

--- components/Projects.js
+++ components/Projects.js
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import Link from "next/link";
+
+
 import React from "react";
 import propertyImg from "../public/assets/projects/blog.png";
 import cryptoImg from "../public/assets/projects/blog.png";
